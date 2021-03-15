930bc41a14fd6330a8315eaaf2115554e3adbac5
Aymen ABED <aymen.abed@metis-innovations.com>
2021-03-15 19:08:11 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	MONGODB:
+		"mongodb+srv://aymen:gfVrBXHYaLNzT9n@cluster0.8q8xa.mongodb.net/meng?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true",
+	SECRET_KEY: "some very secret key",
+};
